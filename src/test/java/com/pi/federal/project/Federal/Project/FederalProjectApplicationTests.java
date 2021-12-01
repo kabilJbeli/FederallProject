b8cdd44c65c96d716f54ada79701412f8b02610e
kabil <kabiljbeli011@gmail.com>
2021-12-01 20:29:58 +0100

    Initial Commit

---
+++ src/test/java/com/pi/federal/project/Federal/Project/FederalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.federal.project.Federal.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
